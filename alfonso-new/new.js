2b2bffacc1a18c6e2ce572b40f8ebb6290f52504
Pochobs <pochobs@hotmail.com>
2020-07-08 19:20:35 -0500

    newdisplay

---
+++ alfonso-new/new.js
@@ -0,0 +1,15 @@
+(function(exports) {
+    "use strict";
+
+    function initMap() {
+      exports.map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+          lat: -34.397,
+          lng: 150.644
+        },
+        zoom: 8
+      });
+    }
+
+    exports.initMap = initMap;
+  })((this.window = this.window || {}));
