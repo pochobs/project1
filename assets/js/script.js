6217bbc6d2ded4b650a941f50a5cbc7d3944827b
ElK <62822011+evkonradi@users.noreply.github.com>
2020-07-10 12:25:08 -0500

    added header and footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -198,17 +198,12 @@ var initMap = function(coordinates, houseInfo) {
 }
 
 $( ".button" ).click(function( event ) {
-    //     alert( "Handler for .submit() called." );
-        // var submitedValues = function() { 
         event.preventDefault();        
         var searchCity = document.querySelector("#search-city").value;
         var searchState = document.querySelector("#search-state").value;
         var searchZipcode = document.querySelector("#search-zipcode").value;
         console.log(searchZipcode, searchCity, searchState,)
         getHousesList(searchZipcode, searchCity, searchState);
-        //initMap();
-        
-    
 });
 
 // call to put multiple listings on the map; list of houses is taken from the local storage
